040b8aa3721a3d9fa46f79544f3b0b7ac61c3669
pirxwes <ynsmrycl2007@gmail.com>
2022-11-06 00:14:54 -0400

    Add game.js file to order program, Color the goal area

--- src/sketch.js
+++ src/sketch.js
@@ -1,14 +1,10 @@
 let isPaused = false;
 
-let players = [];
 let numPlayers = 100;
-let startingLocationX = 300, startingLocationY = 300;
 
 let gameX = null, gameY = null;
 
-let expectedRangeX = null, expectedRangeY = null;
-
-let goal = null;
+let game = null;
 
 function setup(){
 
@@ -16,24 +12,7 @@ function setup(){
 
     gameX = windowWidth - 100, gameY = windowHeight;
 
-    goal = [gameX / 2 - 100, gameY - 100, gameX / 2 + 100, gameY];
-    expectedRangeX = [25, gameX - 25];
-    expectedRangeY = [25, gameY - 25];
-
-    for(let i = 0; i < numPlayers; i++){
-
-        let randX = Math.random() * (expectedRangeX[1] - expectedRangeX[0]) + expectedRangeX[0];
-        let randY = Math.random() * (expectedRangeY[1] - expectedRangeY[0]) + expectedRangeY[0];
-
-        if(randX + 25 >= goal[0] && randX - 25 <= goal[2]){
-            let newExpectedRangeY = [25, goal[1] - 25];
-            randY = Math.random() * (newExpectedRangeY[1] - newExpectedRangeY[0]) + newExpectedRangeY[0];
-        }
-
-
-        let newPlayer = new Player(randX, randY, Array((i % 2 == 0) * 255, 0, (i % 2 != 0) * 255, 255), i % 2 == 0, 25);
-        players.push(newPlayer);
-    }
+    game = new Game(gameX, gameY, numPlayers);
 
 }
 
@@ -49,14 +28,7 @@ function draw(){
     stroke(255);
     rect(0, 0, gameX, gameY);
 
-    stroke(255);
-    rect(goal[0], goal[1], goal[2] - goal[0], goal[3] - goal[1]);
-
-    noStroke();
-    for(let i = 0; i < numPlayers; i++){
-        players[i].play(mouseX, mouseY);
-    }
-
+    game.drawGame(mouseX, mouseY);
     
 }
 
