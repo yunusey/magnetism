08e81e09aa18e2a229c0895b8daf49fc6b7c2bc0
pirxwes <ynsmrycl2007@gmail.com>
2022-11-06 12:05:59 -0500

    Add friction to the goal area

--- src/player.js
+++ src/player.js
@@ -17,35 +17,57 @@ class Player{
     play(xCord, yCord){
 
         if(this.isInTheGoal){
+            console.log("A Big Problem!")
             this.drawBall();
             return;
-        }
-
-        let xDiff = xCord - this.x, yDiff = yCord - this.y;
-        let hDiff = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
-
-        if(hDiff){
-            let hForce = (this.pole ? 1 : -1) * Math.min(100 / Math.pow(hDiff, 2), 0.05);
-            let xForce = hForce * (xDiff / Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2)));
-            let yForce = hForce * (yDiff / Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2)));
-            this.velX += xForce;
-            this.velY += yForce;
-            this.x += this.velX;
-            this.y += this.velY;
-        }
-        if(this.x - this.r <= 0 || this.x + this.r >= this.game.gameX){
-            this.velX = -this.velX;
-        }
-        
-        if(this.y - this.r < 0 || this.y + this.r > this.game.gameY){
-            this.velY = -this.velY;
         }
 
         if(this.x - this.r >= this.game.goal[0] && 
             this.x + this.r <= this.game.goal[2] && 
             this.y - this.r > this.game.goal[1]){
 
-            this.isInTheGoal = true;
+
+            let hForce = -0.2;
+            let hVel = Math.sqrt(Math.pow(this.velX, 2) + Math.pow(this.velY, 2));
+            let xFrict = hForce * this.velX;
+            let yFrict = hForce * this.velY;
+
+
+            this.velX += xFrict;
+            this.velY += yFrict;
+            console.log(xFrict, this.velX);
+        }
+
+        else{
+
+            let xDiff = xCord - this.x, yDiff = yCord - this.y;
+            let hDiff = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+
+            if(hDiff){
+                let hForce = (this.pole ? 1 : -1) * Math.min(100 / Math.pow(hDiff, 2), 0.05);
+                let xForce = hForce * (xDiff / Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2)));
+                let yForce = hForce * (yDiff / Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2)));
+
+
+                this.velX += xForce;
+                this.velY += yForce;
+            }
+            
+        
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+        if(this.x - this.r <= 0 || this.x + this.r >= this.game.gameX){
+            this.velX = -this.velX;
+        }
+        
+        if(this.y - this.r < 0 || this.y + this.r >= this.game.gameY){
+            if((this.y - this.r < 0 && this.velY < 0) || 
+            (this.y + this.r >= this.game.gameY && this.velY > 0)){
+                this.velY = -this.velY;
+                console.log(this.velY);
+            }
         }
 
         this.drawBall();
